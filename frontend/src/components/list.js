819a11ea76179d4c49159669007583509d1b25ac
Vasconcelos <vasconcelos@tarkuss.com.br>
2021-11-21 17:08:23 -0400

    Implement list

---
+++ frontend/src/components/list.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'semantic-ui-react';
+
+export default function List() {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:5000/todo`)
+            .then((response) => {
+                console.log(response.data)
+                setAPIData(response.data);
+            })
+    }, []);
+
+    return (
+        <div>
+            <h2>Tarefas</h2>
+            <Table singleLine>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Título</Table.HeaderCell>
+                        <Table.HeaderCell>Descrição</Table.HeaderCell>
+                        <Table.HeaderCell>Status</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {APIData.map((data) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell>{data.title}</Table.Cell>
+                                <Table.Cell>{data.description}</Table.Cell>
+                                <Table.Cell>{data.done ? 'Feito' : 'Pendente'}</Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+            </Table>
+        </div>
+    )
+}
