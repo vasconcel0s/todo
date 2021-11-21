7b710443ec43fcfd629b2f608ef07a616cae1f0f
Vasconcelos <vasconcelos@tarkuss.com.br>
2021-11-21 19:49:09 -0400

    Dockerize the entire application

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ connex_app.add_api("swagger.yml")
 app = connex_app.app
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root@db:3306/todo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root@todo-db:3306/todo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 
